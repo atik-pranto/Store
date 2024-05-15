62239790d3f7a589f51a059216e165d61af79925
atik <atik1503010@gmail.com>
2024-05-15 22:20:22 +0600

    product entity added for store and seed initial data

---
+++ ecommerce/Data/Migrations/20240515065110_RenameColumn.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ecommerce.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "description",
+                table: "Products",
+                newName: "Description");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Products",
+                newName: "description");
+        }
+    }
+}
