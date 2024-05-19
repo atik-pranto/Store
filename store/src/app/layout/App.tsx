7059e3ed2b29f873ac0f813c38f06c6fa061a32b
atik <atik1503010@gmail.com>
2024-05-19 18:29:50 +0600

    add material ui theme for switching dark and ligth mode

--- store/src/app/layout/App.tsx
+++ store/src/app/layout/App.tsx
@@ -1,19 +1,30 @@
+import { useState } from "react";
 import Catalog from "../../features/catalog/Catalog";
 import Header from "./Header";
 import { Container, CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const paletteType = darkMode ? 'dark' : 'light';
   const theme = createTheme({
     palette: {
-      mode: 'dark'
+      mode: paletteType,
+      background: {
+        default: paletteType === 'light' ? '#eaeaea' : '#121212'
+      }
     }
   });
+
+  function handleThemeChange()
+  {
+    setDarkMode(!darkMode);
+  }
 
   return (
       <>
         <ThemeProvider theme = {theme}>
           <CssBaseline/>
-          <Header/>
+          <Header darkMode = {darkMode} onChange={handleThemeChange}/>
           <Container>
             <Catalog />
           </Container>
--- store/src/app/layout/Header.tsx
+++ store/src/app/layout/Header.tsx
@@ -1,12 +1,22 @@
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import { AppBar, Switch, Toolbar, Typography } from "@mui/material";
 
-export default function Header(){
+type themeProps = {
+    darkMode: boolean;
+    onChange: () => void;
+}
+
+export default function Header({darkMode, onChange}: themeProps){
     return(
         <AppBar position = 'static' sx={{mb: 4}}>
             <Toolbar>
                 <Typography variant='h6'>
                     RE-STORE
                 </Typography>
+                <Switch
+                    checked={darkMode}
+                    onChange={onChange}
+                    inputProps={{ 'aria-label': 'controlled' }}
+                />
             </Toolbar>
         </AppBar>
 
